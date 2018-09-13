90b21bf98d798c843b409e39814f23b3ea2d3095
tavaresrafaela <tavares.rafaela2013@gmail.com>
2018-09-13 19:40:58 -0300

    Adiconando o gravatar

--- events/models.py
+++ events/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from libgravatar import Gravatar
+
 
 class Event (models.Model):
 
@@ -16,3 +19,21 @@ class Event (models.Model):
 
     def __str__(self):
         return self.event
+
+class Coment(models.Model):
+    """Comentário efetuados em um determinado evento."""
+
+    author = models.CharField(max_length=80)
+    email = models.EmailField()
+    texto = models.CharField(max_length=160)
+    commented = models.DateTimeField(default=timezone.now)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE,
+                              related_name='comment_event')
+
+    """Retorna a partir do endereço de email,um avatar configurado"""
+    def avatar (self):
+        g = Gravatar(self.email)
+        return g.get_image(default='identicon')
+
+    def __str__(self):
+        return "{} comentou em {:%c}".format(self.author, self.commented)
