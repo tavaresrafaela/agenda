13e1fa92ccd643935f9e3a75ad2db301a4382aca
tavaresrafaela <tavares.rafaela2013@gmail.com>
2018-09-18 19:56:07 -0300

    Criando a pagina principal

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Event
+from django.utils.timezone import localdate
 
 # Create your views here.
+def index(request):
+    """Exibe a página principal da aplicação"""
+    context= {
+        'hider_new_button': True,
+        'priorities': Event.priorities_list,
+        'today': localdate(),
+    }
+    return render(request, 'index.html', context)
+
