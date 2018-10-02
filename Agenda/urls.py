c6fda46807b399eaedf0f6ba5b6d0a937241314d
tavaresrafaela <tavares.rafaela2013@gmail.com>
2018-10-02 20:47:30 -0300

    Alterando URLs

--- Agenda/urls.py
+++ Agenda/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', include('events.urls')),
+    path('events/', include('events.urls')),
 ]
